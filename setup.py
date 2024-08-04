105826f6f1e3430bea35ff74a3dfeb9b77122a14
Mohamad Abdulwahid <mohamd.abdulwahid@outlook.com>
2024-08-04 16:59:26 +0200

    made some changed to setuptools script

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ import pathlib
 from setuptools import setup
 
 HERE = pathlib.Path(__file__).parent
-README = (HERE / "QSwitchControl\\README.md").read_text()
+README = (HERE / "README.md").read_text()
 setup(
 	name="QSwitchControl",
 	version="1.0.4",
 	description="An easy-to-use and modern toggle switch for Qt Python binding PyQt",
 	long_description=README,
 	long_description_content_type="text/markdown",
-	url="https://github.com/Prx001/QSwitchControl",
-	author="Parsa.py",
-	author_email="munichbayern2005@gmail.com",
+	url="https://github.com/M1GW/QSwitchControls",
+	author="M1GW",
 	license="MIT",
 	classifiers=[
 		"License :: OSI Approved :: MIT License",
@@ -23,5 +22,5 @@ setup(
 	# py_modules=["__init__", "__main__", "QSwitchControl", "QSwitchControlplugin"],
 	packages=["QSwitchControl"],
 	include_package_data=True,
-	install_requires=["PyQt5"]
+	install_requires=["PyQt6"]
 )
